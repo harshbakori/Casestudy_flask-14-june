706b3b57f11eb4827847b8eea2366ee873bd37e9
D3@D53C71 <30556333+harshbakori@users.noreply.github.com>
2020-06-17 15:00:00 +0530

    signup form using wtf form done

---
+++ application/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,IntegerField,TextAreaField,SelectField,SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError,NumberRange
+# from application.models import User
+
+class RegisterForm(FlaskForm):
+
+    state_pairs=[("New York","New York"),("Texas","Texas"),("Utah","Utah"),("Virginia","Virginia"),("Missouri","Missouri"),("Vermont","Vermont")]
+    city_pairs=[("New York","New York"),("Texas","Texas"),("Utah","Utah"),("Virginia","Virginia"),("Missouri","Missouri"),("Vermont","Vermont")]
+
+    SSN_ID = StringField("SSN ID", validators=[DataRequired(),Length(min=2,max=55)])
+    Cust_Name = StringField("Customer Name", validators=[DataRequired(),Length(min=2,max=55)])
+    Cust_Age = IntegerField("Customer Age",validators=[DataRequired(),NumberRange(min=18, max=80)])
+    address = TextAreaField("Address", validators=[DataRequired(),Length(min=2,max=55)])
+    State = SelectField("State",validators=[DataRequired()],choices=state_pairs)
+    City = SelectField("State",validators=[DataRequired()],choices=city_pairs)
+    submit = SubmitField("Submit")
--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,11 @@
 from application import app
-from flask import render_template
+from flask import render_template, request, json, jsonify, Response, redirect, flash, url_for, session
+from application.forms import RegisterForm
 
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template("index.html")
+    form = RegisterForm()
+    
+    return render_template("CreateAccount.html",form=form,home=True)
